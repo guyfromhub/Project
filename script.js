705ba250631781e3632ed001ff83f6018a7f410c
guyfromhub <priyanshudas1316@gmail.com>
2023-12-30 16:14:31 +0530

    updates

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-//alert("Hello World")
+var car = ["Mercedes", "Bugatti", "Nissan", "Lamborghini"];
 
-//fullName=prompt("Enter Your Name:")
-
-var a = 0;
-var b = 1;
-
-var d = 1000;
-for (let a = 1; a <= 1000; a = b) {
-  b = a + b;
-  console.log(b);
-}
+console.log(car);
+Array.from("Raj");
+console.log(Array.from("Hello"));
