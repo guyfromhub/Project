2e74be0ba19b0d2dbc836af96843de1430b2da09
guyfromhub <priyanshudas1316@gmail.com>
2024-01-12 21:31:00 +0530

    Usables

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-var car = ["Mercedes", "Bugatti", "Nissan", "Lamborghini"];
+// Array of objects representing information about persons
+let persons = [
+  {
+    name: "John",
+    age: 25,
+    city: "New York",
+  },
+  {
+    name: "Alice",
+    age: 30,
+    city: "San Francisco",
+  },
+  {
+    name: "Bob",
+    age: 22,
+    city: "Los Angeles",
+  },
+];
 
-console.log(car);
-Array.from("Raj");
-console.log(Array.from("Hello"));
+// Accessing and displaying information from the array
+for (let i = 0; i < persons.length; i++) {
+  console.log("Name: " + persons[i].name);
+  console.log("Age: " + persons[i].age);
+  console.log("City: " + persons[i].city);
+}
